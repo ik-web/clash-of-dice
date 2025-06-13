a1382fca03e78720b4ceb92f9306683d9c24a88d
Ihor Kuchin <i.kuchin@yaware.com>
2025-06-13 12:16:15 +0300

    add dnd 5e api service

---
+++ src/services/dnd-api.js
@@ -0,0 +1,50 @@
+const BASE_URL = 'https://www.dnd5eapi.co/api';
+
+async function fetchFromApi(endpoint) {
+    try {
+        const response = await fetch(`${BASE_URL}${endpoint}`);
+
+        if (!response.ok) {
+            throw new Error(`Error ${response.status}: ${response.statusText}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error(`Failed to fetch ${endpoint}:`, error.message);
+        throw error;
+    }
+}
+
+export const dndApiService = {
+    getAllClasses() {
+        return fetchFromApi('/classes');
+    },
+
+    getClassByIndex(index) {
+        return fetchFromApi(`/classes/${index}`);
+    },
+
+    getAllSpells() {
+        return fetchFromApi('/spells');
+    },
+
+    getSpellByIndex(index) {
+        return fetchFromApi(`/spells/${index}`);
+    },
+
+    getMonsterList() {
+        return fetchFromApi('/monsters');
+    },
+
+    getMonsterByIndex(index) {
+        return fetchFromApi(`/monsters/${index}`);
+    },
+
+    getRaceList() {
+        return fetchFromApi('/races');
+    },
+
+    getRaceByIndex(index) {
+        return fetchFromApi(`/races/${index}`);
+    },
+};
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,53 @@
 <template>
-    <main>Hi</main>
+    <div class="home-page">
+        <main>
+            <div class="monsters">
+                <ul class="monsters__list">
+                    <li
+                        class="monster"
+                        v-for="(monster, i) in monsters"
+                        :key="monster"
+                    >
+                        <button
+                            class="monster__btn"
+                            @click="onMonsterBtnClick(monster.index)"
+                        >
+                            {{ monster.name }}
+                        </button>
+                    </li>
+                </ul>
+
+                <div
+                    v-if="selectedMonster"
+                    class="selected-monster"
+                >
+                    <div
+                        v-show="loading"
+                        class="selected-monster__loading"
+                    >
+                        Loading...
+                    </div>
+
+                    <div class="selected-monster__content">
+                        <img
+                            v-show="!loading"
+                            class="selected-monster__img"
+                            alt="Monster image"
+                            :src="`https://www.dnd5eapi.co${selectedMonster.image}`"
+                            @load="onImageLoad"
+                        />
+
+                        <h2
+                            v-show="!loading"
+                            class="selected-monster__title"
+                        >
+                            {{ selectedMonster.name }}
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
 </template>
 
 <script src="./index.js"></script>
