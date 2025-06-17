b833c303ff8c9dada777b118f2d5563957ac0aa8
Ihor_Kuchin <vedunigor@gmail.com>
2025-06-17 15:48:58 +0300

    project rename

--- src/components/layout/PageLayout.vue
+++ src/components/layout/PageLayout.vue
@@ -1,31 +1,27 @@
 <template>
-    <div class="page-layout">
-        <header class="page-layout__header">
-            <div class="page-layout__logo">
-                <img
-                    class="page-layout__logo-img"
-                    alt="Logo"
-                    :src="logoHref"
-                />
-                <span class="page-layout__logo-text">Monster fighting</span>
-            </div>
+	<div class="page-layout">
+		<header class="page-layout__header">
+			<div class="page-layout__logo">
+				<img class="page-layout__logo-img" alt="Logo" :src="logoHref" />
+				<span class="page-layout__logo-text">Clash of Dice</span>
+			</div>
 
-            <nav class="page-layout__nav">
-                <router-link
-                    v-for="page in pages"
-                    :key="page.path"
-                    :to="page.path"
-                    :title="page.name"
-                >
-                    {{ page.name }}
-                </router-link>
-            </nav>
-        </header>
+			<nav class="page-layout__nav">
+				<router-link
+					v-for="page in pages"
+					:key="page.path"
+					:to="page.path"
+					:title="page.name"
+				>
+					{{ page.name }}
+				</router-link>
+			</nav>
+		</header>
 
-        <main class="page-layout__main">
-            <slot></slot>
-        </main>
-    </div>
+		<main class="page-layout__main">
+			<slot></slot>
+		</main>
+	</div>
 </template>
 
 <script src="./index.js"></script>
