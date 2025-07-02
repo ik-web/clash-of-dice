d0ed4235bf6f51011f5a0fa42289b362a98f2932
Ihor_Kuchin <vedunigor@gmail.com>
2025-07-02 14:45:55 +0300

    add not found page

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,68 +7,74 @@ import BestiaryView from '@/views/bestiary/BestiaryView.vue';
 import SettingsView from '@/views/settings/SettingsView.vue';
 import EncountersView from '@/views/encounters/EncountersView.vue';
 import EncounterView from '@/views/encounter/EncounterView.vue';
+import NotFoundView from '@/views/not-found/NotFoundView.vue';
 
 export const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: HomeView,
-        meta: {
-            navLabel: 'Home',
-        },
-    },
-    {
-        path: '/encounters',
-        name: 'encounters',
-        component: EncountersView,
-        meta: {
-            navLabel: 'Encounters',
-            subNavigation: subNavigation.encounters,
-        },
-    },
-    {
-        path: '/encounters/encounter/:id',
-        name: 'encounters-encounter',
-        component: EncounterView,
-        meta: {
-            subNavigation: subNavigation.encounters,
-        },
-    },
-    {
-        path: '/encounters/combat/:id',
-        name: 'encounters-combat',
-        component: CombatView,
-    },
-    // {
-    //     path: '/parties',
-    //     name: 'parties',
-    //     component: PartiesView,
-    //     meta: {
-    //         navLabel: 'Parties',
-    //     },
-    // },
-    // {
-    //     path: '/bestiary',
-    //     name: 'bestiary',
-    //     component: BestiaryView,
-    //     meta: {
-    //         navLabel: 'Bestiary',
-    //     },
-    // },
-    // {
-    //     path: '/score',
-    //     name: 'score',
-    //     component: ScoreView,
-    //     meta: {
-    //         navLabel: 'Score',
-    //     },
-    // },
-    {
-        path: '/settings',
-        name: 'settings',
-        component: SettingsView,
-        meta: {
-            navLabel: 'Settings',
-        },
-    },
+	{
+		path: '/',
+		name: 'home',
+		component: HomeView,
+		meta: {
+			navLabel: 'Home',
+		},
+	},
+	{
+		path: '/encounters',
+		name: 'encounters',
+		component: EncountersView,
+		meta: {
+			navLabel: 'Encounters',
+			subNavigation: subNavigation.encounters,
+		},
+	},
+	{
+		path: '/encounters/encounter/:id',
+		name: 'encounters-encounter',
+		component: EncounterView,
+		meta: {
+			subNavigation: subNavigation.encounters,
+		},
+	},
+	{
+		path: '/encounters/combat/:id',
+		name: 'encounters-combat',
+		component: CombatView,
+	},
+	// {
+	//     path: '/parties',
+	//     name: 'parties',
+	//     component: PartiesView,
+	//     meta: {
+	//         navLabel: 'Parties',
+	//     },
+	// },
+	// {
+	//     path: '/bestiary',
+	//     name: 'bestiary',
+	//     component: BestiaryView,
+	//     meta: {
+	//         navLabel: 'Bestiary',
+	//     },
+	// },
+	// {
+	//     path: '/score',
+	//     name: 'score',
+	//     component: ScoreView,
+	//     meta: {
+	//         navLabel: 'Score',
+	//     },
+	// },
+	{
+		path: '/settings',
+		name: 'settings',
+		component: SettingsView,
+		meta: {
+			navLabel: 'Settings',
+		},
+	},
+	{
+		path: '/:pathMatch(.*)*',
+		name: 'not-found',
+		component: NotFoundView,
+	},
 ];
---
+++ src/views/not-found/NotFoundView.vue
@@ -0,0 +1,14 @@
+<template>
+	<page-layout>
+		<div class="not-found">
+			<h1 class="not-found__title">404</h1>
+			<p class="not-found__subtitle">Oops... page not found</p>
+			<p class="not-found__message">
+				The link you followed probably broken or the page has been removed.
+			</p>
+		</div>
+	</page-layout>
+</template>
+
+<script src="./index.js"></script>
+<style src="./index.scss" scoped></style>
